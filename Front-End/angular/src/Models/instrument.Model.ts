dba667680e7a5e458ace3c3ae3f44ce617bf1127
Manimaran Venkat <Manimaran.0610@gmail.com>
2023-10-19 16:40:14 +0530

    Updated breadcrumbs and dialog popups

---
+++ Front-End/angular/src/Models/instrument.Model.ts
@@ -0,0 +1,8 @@
+export class Instrument {
+    serialNo: string='';
+    make: string='';
+    model: string='';
+    calibratedOn?: Date;
+    calibratedDueOn?: Date;
+    certificate: string='';
+}
--- Front-End/angular/src/app/demo/Instruments/add-instrument/add-instrument.component.ts
+++ Front-End/angular/src/app/demo/Instruments/add-instrument/add-instrument.component.ts
@@ -1,22 +1,32 @@
-// import { ConfirmDialogModule } from 'primeng/confirmdialog';
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ConfirmationService } from 'primeng/api/confirmationservice';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConfirmationService, Message } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { Instrument } from 'src/Models/instrument.Model';
+
 
 @Component({
   selector: 'app-add-instrument',
-  standalone:true,
-  imports:[CommonModule,SharedModule],
+  standalone: true,
+  imports: [CommonModule, SharedModule, ConfirmDialogModule, MessagesModule],
+  providers: [ConfirmationService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './add-instrument.component.html',
   styleUrls: ['./add-instrument.component.css']
 })
 export class AddInstrumentComponent implements OnInit {
 
-  instrumentFormGroup:FormGroup;
-  constructor() { 
+  instrumentFormGroup: FormGroup;
+  msgs: Message[] = [];
+  isAddInstrument: boolean = true;
+  instrumentModel: Instrument = new Instrument();
+
+  constructor(private confirmationService: ConfirmationService) {
+
     this.instrumentFormGroup = new FormGroup(
       {
         serialNo: new FormControl(),
@@ -25,32 +35,42 @@ export class AddInstrumentComponent implements OnInit {
         calibratedOn: new FormControl(),
         calibratedDueOn: new FormControl(),
         certificate: new FormControl(),
-   
-
       }
-    )
+    );
+    this.addFormControlValidators();
   }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onSubmit() {
+    console.log("Form submitted")
+    this.instrumentModel = this.instrumentFormGroup.value;
+    console.log(this.instrumentModel)
+    this.confirmDialog();
   }
 
-  onSubmit(){
-    // this.confirmationService.confirm({
-    //   message: 'Are you sure that you want to proceed?',
-    //   header: 'Confirmation',
-    //   icon: 'pi pi-exclamation-triangle',
-    //   accept: () =>    console.log(this.instrumentFormGroup.value)      ,
-    //   reject: () =>console.log("Reject")
-    // });
-    console.log("on submit")
+  onClear() { this.instrumentFormGroup.reset() }
 
-    console.log(this.instrumentFormGroup.value)
+  confirmDialog() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to save instrument?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptButtonStyleClass: 'btn btn-success m-2',
+      rejectButtonStyleClass: 'btn btn-danger',
+      accept: () => {
+        this.msgs = [{ severity: 'success', summary: 'Success', detail: 'Instrument added', life: 3000 }];
+        alert('Following Data will be posted to Server Database\n' + JSON.stringify(this.instrumentModel));
+      },
+      reject: () => {
+        this.msgs = [{ severity: 'error', summary: 'Failed', detail: 'Instrument not added', life: 3000 }];
+      }
+    });
   }
 
-  public onClear(){
-    this.instrumentFormGroup.reset()
-    console.log("on clear")
-    console.log(this.instrumentFormGroup.value)
-
+  addFormControlValidators(){
+    this.instrumentFormGroup.controls['serialNo'].addValidators([Validators.required])
   }
+
+  toggle() { this.isAddInstrument = !this.isAddInstrument; }
 }
