263031ac1ec22909440739dfd5f45941fc06ef3f
Manimaran Venkat <Manimaran.0610@gmail.com>
2023-10-28 13:18:09 +0530

    Added file validation and included toast messages

--- Front-End/angular/src/app/demo/Instruments/add-instrument/add-instrument.component.ts
+++ Front-End/angular/src/app/demo/Instruments/add-instrument/add-instrument.component.ts
@@ -4,20 +4,22 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ConfirmationService, Message } from 'primeng/api';
+import { ConfirmationService, Message, MessageService } from 'primeng/api';
 import { MessagesModule } from 'primeng/messages';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { Instrument } from 'src/Models/instrument.Model';
 import { InstrumentService } from 'src/Services/Instrument.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
 import { BaseResponse } from 'src/Models/response-models/base-response';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
     selector: 'app-add-instrument',
     standalone: true,
-    imports: [CommonModule, SharedModule, ConfirmDialogModule, MessagesModule, HttpClientModule],
-    providers: [ConfirmationService, InstrumentService, BaseHttpClientServiceService],
+    imports: [CommonModule, SharedModule, ConfirmDialogModule, MessagesModule, HttpClientModule, ToastModule],
+    providers: [ConfirmationService, InstrumentService, BaseHttpClientServiceService, MessageService],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     templateUrl: './add-instrument.component.html',
     styleUrls: ['./add-instrument.component.css']
@@ -31,7 +33,12 @@ export class AddInstrumentComponent implements OnInit {
     tempFile?: string;
     instrumentModel: Instrument = new Instrument();
 
-    constructor(private confirmationService: ConfirmationService, private instrumentService: InstrumentService) {
+    constructor(
+        private confirmationService: ConfirmationService,
+        private instrumentService: InstrumentService,
+        private messageService: MessageService,
+        private router: Router
+    ) {
 
         this.instrumentFormGroup = new FormGroup(
             {
@@ -50,11 +57,15 @@ export class AddInstrumentComponent implements OnInit {
 
     async onSubmit() {
         console.log("Form submitted")
-        this.instrumentModel = this.instrumentFormGroup.value;
-        this.instrumentModel.certificateName = this.tempFileName;
-        this.instrumentModel.CertificateFile = this.tempFile;
-        console.log(this.instrumentModel)
-        this.confirmDialog();
+        if (this.tempFileName !== undefined && this.tempFile !== undefined) {
+            this.instrumentModel = this.instrumentFormGroup.value;
+            this.instrumentModel.certificateName = this.tempFileName;
+            this.instrumentModel.CertificateFile = this.tempFile;
+            this.confirmDialog();
+        } else {
+            this.messageService.add({ key: 'tc', severity: 'info', summary: 'Required', detail: "choose the certificate file", life: 4000 });
+        }
+
     }
 
     onClear() { this.instrumentFormGroup.reset() }
@@ -69,32 +80,39 @@ export class AddInstrumentComponent implements OnInit {
             accept: () => {
                 this.postInstrumentToAPIServer()
             },
-            reject: () => {
-                this.msgs = [{ severity: 'error', summary: 'Failed', detail: 'Instrument not added', life: 3000 }];
-            }
+            reject: () => { }
         });
     }
 
     postInstrumentToAPIServer() {
         this.instrumentService.postInstrument(this.instrumentModel).subscribe({
             next: (response: BaseResponse<number>) => {
-                this.msgs = [{ severity: 'success', summary: 'Success', detail: 'Instrument created', life: 3000 }];
+                if (response.succeeded) this.messageService.add({ key: 'tc', severity: 'success', summary: 'Success', detail: 'Instrument created', life: 4000 });
+
             },
             error: (e) => {
-                this.msgs = [{ severity: 'error', summary: 'Failed', detail: e.error.message, life: 3000 }];
+                this.messageService.add({ key: 'tc', severity: 'error', summary: 'Failed', detail: e.error.title, life: 4000 });
             },
             complete: () => {
-                console.log('Complete tenant List');
+                // this.router.navigateByUrl("/Home-page")
             },
         });
     }
     async uploadFile(event: any) {
+
         if (event.files.length > 0 && event.files[0].size < 5120000) {
             this.tempFileName = event.files[0].name;
             console.log(event.files[0])
             console.log(this.tempFileName)
             this.tempFile = await this.fileToBase64(event.files[0]);
             console.log(this.tempFile)
+        }
+        else if (event.files.length > 0 && event.files[0].size > 5120000) {
+            this.messageService.add({ key: 'tc', severity: 'warn', summary: 'Not Supported', detail: "File size should be less than 5MB", life: 4000 });
+        }
+        else {
+            this.tempFile = undefined;
+            this.tempFileName = undefined;
         }
     }
 
