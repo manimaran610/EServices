2cd4cf62f506cee918086e2f6e62d2e2ecd8184a
Manimaran Venkat <Manimaran.0610@gmail.com>
2023-11-04 20:15:37 +0530

    Added popup in forms

--- Front-End/angular/src/app/demo/reports/acph/acph.component.ts
+++ Front-End/angular/src/app/demo/reports/acph/acph.component.ts
@@ -4,6 +4,7 @@ import { CustomerDetailsComponent } from '../shared/customer-details/customer-de
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogService, DynamicDialogModule,DynamicDialogRef  } from 'primeng/dynamicdialog';
 import { MessagesModule } from 'primeng/messages';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastModule } from 'primeng/toast';
@@ -14,20 +15,32 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-acph',
   standalone: true,
-  imports: [CommonModule, SharedModule, ConfirmDialogModule, MessagesModule, HttpClientModule, ToastModule, CustomerDetailsComponent],
-  providers: [ConfirmationService, BaseHttpClientServiceService, MessageService],
+  imports: [CommonModule, SharedModule, ConfirmDialogModule, MessagesModule, HttpClientModule, ToastModule, CustomerDetailsComponent,DynamicDialogModule],
+  providers: [ConfirmationService, BaseHttpClientServiceService, MessageService,DialogService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './acph.component.html',
   styleUrls: ['./acph.component.css']
 })
 export class AcphComponent implements OnInit {
+  ref: DynamicDialogRef | undefined;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,public dialogService: DialogService) { }
+
+  show() {
+    this.ref = this.dialogService.open(CustomerDetailsComponent, {
+      header: 'Select a Product',
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: false
+  });
+  }
 
   ngOnInit() {
   }
   navigateToUrl = (url: string | undefined) => {
      this.router.navigateByUrl(url!);
   }
+  
 
 }
