4ddbd3eee41e6d41926e451f652bd540660f49a1
Manimaran Venkat <Manimaran.0610@gmail.com>
2023-11-24 07:29:35 +0530

    Update HttpLoggingMiddleware.cs

--- Back-End/EServices.WebApi/WebApi/Middlewares/HttpLoggingMiddleware.cs
+++ Back-End/EServices.WebApi/WebApi/Middlewares/HttpLoggingMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -19,6 +21,7 @@ namespace WebApi.Middlewares
     {
         private readonly ILogger _logger;
         private readonly RequestDelegate _next;
+        private readonly List<string> _excludePaths = new() { "Log" };
 
         public HttpLoggingMiddleware(RequestDelegate next, ILogger<HttpLoggingMiddleware> logger)
         {
@@ -28,33 +31,41 @@ namespace WebApi.Middlewares
 
         public async Task Invoke(HttpContext context)
         {
-            var start = Stopwatch.GetTimestamp();
+            if (_excludePaths.Any(e => context.Request.Path.Value.Contains(e)))
+            {
+                //Continue down the Middleware pipeline
+                await _next(context);
+            }
+            else
+            {
+                var start = Stopwatch.GetTimestamp();
 
-            //Copy  pointer to the original response body stream
-            var originalBodyStream = context.Response.Body;
+                //Copy  pointer to the original response body stream
+                var originalBodyStream = context.Response.Body;
 
-            //Get incoming request
-            var request = await GetRequestAsTextAsync(context.Request);
-            //Log it
-            // Serilog.Log.Information(request);
+                //Get incoming request
+                var request = await GetRequestAsTextAsync(context.Request);
+                //Log it
+                // Serilog.Log.Information(request);
 
 
-            //Create a new memory stream and use it for the temp reponse body
-            await using var responseBody = new MemoryStream();
-            context.Response.Body = responseBody;
+                //Create a new memory stream and use it for the temp reponse body
+                await using var responseBody = new MemoryStream();
+                context.Response.Body = responseBody;
 
-            //Continue down the Middleware pipeline
-            await _next(context);
+                //Continue down the Middleware pipeline
+                await _next(context);
 
-            //Format the response from the server
-            var response = await GetResponseAsTextAsync(context.Response);
-            //Log it
-            // _logger.LogInformation(response);
+                //Format the response from the server
+                var response = await GetResponseAsTextAsync(context.Response);
+                //Log it
+                // _logger.LogInformation(response);
 
-            //Copy the contents of the new memory stream, which contains the response to the original stream, which is then returned to the client.
-            await responseBody.CopyToAsync(originalBodyStream);
-            var elapsed = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
-            LogRequestResponse(context, elapsed, request, response);
+                //Copy the contents of the new memory stream, which contains the response to the original stream, which is then returned to the client.
+                await responseBody.CopyToAsync(originalBodyStream);
+                var elapsed = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
+                LogRequestResponse(context, elapsed, request, response);
+            }
         }
 
 
@@ -62,7 +73,7 @@ namespace WebApi.Middlewares
         {
             LogContext.PushProperty("QueryString", context.Request.QueryString);
             LogContext.PushProperty("StatusCode", context.Response.StatusCode);
-            LogContext.PushProperty("ClientIp",context.Connection.RemoteIpAddress);
+            LogContext.PushProperty("ClientIp", context.Connection.RemoteIpAddress);
             LogContext.PushProperty("Request", request);
             LogContext.PushProperty("Response", response);
             LogContext.PushProperty("Elapsed", elapsed);
@@ -84,7 +95,7 @@ namespace WebApi.Middlewares
             return text;
         }
 
-         private async Task<string> GetRequestAsTextAsync(HttpRequest request)
+        private async Task<string> GetRequestAsTextAsync(HttpRequest request)
         {
             // Ensure the request's body can be read multiple times 
             // (for the next middlewares in the pipeline).
